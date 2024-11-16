008ed4efd78358db6d09c3841068177bb5e08407
meaton96 <lammara96@gmail.com>
2024-11-16 11:14:31 -0500

    init and project structure

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    entry: { //TODO FIX!!!!!!!!
+        app: './client/maker.jsx',
+        login: './client/login.jsx',
+        battle: './client/battle.jsx',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                },
+            },
+        ],
+    },
+    mode: 'production',
+    watchOptions: {
+        aggregateTimeout: 200,
+    },
+    output: {
+        path: path.resolve(__dirname, 'hosted'),
+        filename: '[name]Bundle.js',
+    },
+    devServer: {
+        hot: true,
+    },
+};
